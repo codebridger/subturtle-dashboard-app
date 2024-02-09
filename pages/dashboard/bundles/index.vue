5bbdbb0cb2eda3d7540900db3ea8fbdd4f9548cd
Navid Shad <navidshad72@gmail.com>
2024-02-09 21:13:23 +0300

    Add filter and button components to bundles page

--- pages/dashboard/bundles/index.vue
+++ pages/dashboard/bundles/index.vue
@@ -5,6 +5,30 @@ definePageMeta({
   title: "Phrase Bundles",
   layout: "collapse",
 });
+
+const filter = ref("");
+const perPage = ref(10);
 </script>
 
-<template></template>
+<template>
+  <TairoContentWrapper>
+    <template #left>
+      <BaseInput
+        v-model="filter"
+        icon="lucide:search"
+        placeholder="Filter bundles..."
+        :classes="{
+          wrapper: 'w-full sm:w-auto',
+        }"
+      />
+    </template>
+    <template #right>
+      <BaseButton color="primary" class="w-full" disabled>
+        <Icon name="lucide:plus" class="h-4 w-4" />
+        <span>Add New</span>
+      </BaseButton>
+    </template>
+
+    <div>dasdasd</div>
+  </TairoContentWrapper>
+</template>
