ad9bdf9a957a049a62cb3ab6a461be9811795a53
Navid Shad <navidshad72@gmail.com>
2024-02-11 13:07:40 +0300

    fetch bundles with pagination

--- pages/dashboard/bundles/index.vue
+++ pages/dashboard/bundles/index.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { dataProvider } from "@modular-rest/client";
+import type { PaginationType } from "@modular-rest/client/dist/types/types";
+import {
+  COLLECTIONS,
+  DATABASE,
+  type PhraseBundleType,
+} from "~/types/database.type";
+
 definePageMeta({
   // static meta information can be added to vue-router, we use it
   // to generate the search index in the demo
@@ -6,7 +14,36 @@ definePageMeta({
 });
 
 const filter = ref("");
-const perPage = ref(10);
+const perPage = ref(2);
+const bundleList = ref<PhraseBundleType[]>([]);
+const pagination = ref<PaginationType | null>(null);
+
+const controller = dataProvider.list<PhraseBundleType>(
+  {
+    database: DATABASE.USER_CONTENT,
+    collection: COLLECTIONS.PHRASE_BUNDLE,
+    query: {
+      refId: authUser.value?.id,
+      title: {
+        $regex: filter.value,
+        $options: "i",
+      },
+    },
+  },
+  {
+    limit: perPage.value,
+    page: 1,
+    onFetched: (data) => {
+      bundleList.value = data;
+      pagination.value = controller.pagination;
+    },
+  }
+);
+
+onMounted(async () => {
+  await controller.updatePagination();
+  controller.fetchPage(1);
+});
 </script>
 
 <template>
@@ -29,7 +66,35 @@ const perPage = ref(10);
         </BaseButton>
       </template>
 
-      <div>dasdasd</div>
+      <section class="ltablet:grid-cols-2 grid w-full gap-4 lg:grid-cols-2">
+        <BaseCard rounded="none" class="p-6" v-for="bundle of bundleList">
+          <BaseHeading
+            as="h4"
+            size="sm"
+            weight="semibold"
+            lead="tight"
+            class="text-muted-800 mb-2 dark:text-white"
+          >
+            {{ bundle.title }}
+          </BaseHeading>
+
+          <!-- <BaseParagraph size="sm" lead="tight" class="text-muted-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </BaseParagraph> -->
+        </BaseCard>
+      </section>
+
+      <BasePagination
+        class="mt-4"
+        v-if="pagination"
+        :item-per-page="controller.pagination.limit"
+        :total-items="controller.pagination.total"
+        :current-page="controller.pagination.page"
+        :max-links-displayed="5"
+        rounded="lg"
+        :no-router="false"
+        @update:currentPage="controller.fetchPage($event)"
+      />
     </TairoContentWrapper>
   </div>
 </template>
---
+++ types/database.type.ts
@@ -0,0 +1,35 @@
+import { type FileDocument } from "@modular-rest/client/dist/types/types";
+
+export const DATABASE = {
+  USER_CONTENT: "user_content",
+};
+
+export const COLLECTIONS = {
+  PHRASE: "phrase",
+  PHRASE_BUNDLE: "phrase_bundle",
+};
+
+export interface PhraseType {
+  _id: string;
+  refId: string;
+  phrase: string;
+  translation: string;
+  translation_language: string;
+  images?: [FileDocument];
+}
+
+export interface PhraseBundleType {
+  _id: string;
+  refId: string;
+  title: string;
+  image: [FileDocument];
+  phrases: [string];
+}
+
+export interface PopulatedPhraseBundleType {
+  _id: string;
+  refId: string;
+  title: string;
+  image: [FileDocument];
+  phrases: [PhraseType];
+}
