df80ad8ba344c4a3b51a3a47e53266523ba1cd76
Navid Shad <navidshad72@gmail.com>
2024-04-28 23:57:41 +0300

    Update ToolbarAccountMenu.vue with transition class and handleLogout function

--- components/global/ToolbarAccountMenu.vue
+++ components/global/ToolbarAccountMenu.vue
@@ -29,6 +29,7 @@ function handleLogout() {
       </MenuButton>
 
       <Transition
+        class="z-[9999]"
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
         enter-to-class="transform scale-100 opacity-100"
