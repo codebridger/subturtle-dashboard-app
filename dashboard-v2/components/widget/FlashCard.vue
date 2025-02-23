2b58513fafbce17fc358502c0fd151a152d78a79
SomiVista <somayerohani06@gmail.com>
2025-02-23 22:06:43 +0300

    #86erfetwd-Add Card component to Flashcard

--- dashboard-v2/components/widget/FlashCard.vue
+++ dashboard-v2/components/widget/FlashCard.vue
@@ -1,14 +1,12 @@
 <template>
-    <Card
-        class="flex h-full w-full cursor-pointer select-none items-center justify-center rounded-lg p-6 shadow-sm"
-        :color="!isFlipped ? 'default' : 'info'"
-        @click="flipCard"
-    >
-        <div class="text-5xl">
-            <span v-if="!isFlipped">{{ props.front || 'Front' }}</span>
-            <span v-else>{{ props.back || 'Back' }}</span>
-        </div>
-    </Card>
+    <div class="h-full w-full cursor-pointer select-none" @click="flipCard">
+        <Card :type="!isFlipped ? 'default' : 'primary'" class="flex h-full items-center justify-center p-6" hover>
+            <div class="text-center text-5xl">
+                <span v-if="!isFlipped">{{ props.front || 'Front' }}</span>
+                <span v-else>{{ props.back || 'Back' }}</span>
+            </div>
+        </Card>
+    </div>
 </template>
 
 <script setup lang="ts">
