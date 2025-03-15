7afcd91a16774dc64b87375a9e50d40bb2360caf
Navid Shad <navidshad72@gmail.com>
2025-03-16 02:21:44 +0400

    fix: update login_with_token component to use loginWithToken method and add error logging on authentication failure

--- pages/auth/login_with_token.vue
+++ pages/auth/login_with_token.vue
@@ -1,30 +1,31 @@
 <template></template>
 
 <script lang="ts" setup>
-  import { authentication } from '@modular-rest/client';
-  import { useRoute, useRouter } from 'vue-router';
-  import { useProfileStore } from '~/stores/profile';
+    import { authentication } from '@modular-rest/client';
+    import { useRoute, useRouter } from 'vue-router';
+    import { useProfileStore } from '~/stores/profile';
 
-  const route = useRoute();
-  const router = useRouter();
-  const profileStore = useProfileStore();
+    const route = useRoute();
+    const router = useRouter();
+    const profileStore = useProfileStore();
 
-  definePageMeta({
-    layout: 'blank',
-  });
+    definePageMeta({
+        layout: 'blank',
+    });
 
-  onMounted(() => {
-    const token = route.query.token as string;
-    if (token) {
-      authentication
-        .loginWithLastSession(token)
-        .then(profileStore.getProfileInfo)
-        .then(() => {
-          router.push('/');
-        })
-        .catch(() => {
-          router.push('/auth/login');
-        });
-    }
-  });
+    onMounted(() => {
+        const token = route.query.token as string;
+        if (token) {
+            authentication
+                .loginWithToken(token)
+                .then(profileStore.getProfileInfo)
+                .then(() => {
+                    router.push('/');
+                })
+                .catch((error) => {
+                    console.error(error);
+                    router.push('/auth/login');
+                });
+        }
+    });
 </script>
