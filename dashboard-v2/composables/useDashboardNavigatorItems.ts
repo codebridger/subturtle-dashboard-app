94be8147685c00d8cb61cf1fbaa468f314ad33d5
Navid Shad <navidshad72@gmail.com>
2025-03-15 21:17:49 +0400

    feat: update dashboard navigator to reduce the nesting complexity, the nvigator dosent has nested items now.

--- dashboard-v2/composables/useDashboardNavigatorItems.ts
+++ dashboard-v2/composables/useDashboardNavigatorItems.ts
@@ -5,23 +5,31 @@ export const useDashboardNavigatorItems = (): Array<SidebarGroupType> => {
 
   return [
     {
-      title: '',
+      title: t('dashboard.nav'),
       children: [
         {
-          title: t('dashboard.nav'),
+          title: t('statistic'),
           icon: 'IconMenuDashboard',
-          child: [
-            { title: t('statistic'), to: '/statistic' },
-            { title: t('membership-plans.title'), to: '/membership-plans' },
-            // { title: t('settings'), to: '' },
-          ],
+          to: '/statistic',
+          // child: [
+          //   { title: t('statistic'), to: '/statistic' },
+          //   // { title: t('membership-plans.title'), to: '/membership-plans' },
+          //   // { title: t('settings'), to: '' },
+          // ],
         },
+
+      ],
+    },
+    {
+      title: t('practice.nav'),
+      children: [
         {
           title: t('bundle.nav'),
           icon: 'IconMenuDatatables',
-          child: [{ title: t('bundle.list'), to: '/bundles' }],
+          to: '/bundles',
+          // child: [{ title: t('bundle.list'), to: '/bundles' }],
         },
-      ],
-    },
-  ];
+      ]
+    }
+  ] as Array<SidebarGroupType>;
 };
