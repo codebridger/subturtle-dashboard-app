6e972b0e1ac13f81246bb95ca15c3c91f845ba7f
Navid Shad <navidshad72@gmail.com>
2025-03-18 19:54:23 +0400

    refactor: enhance live-session component by adding token usage display and updating phrase selection logic for improved clarity and functionality

--- frontend/types/live-session.type.ts
+++ frontend/types/live-session.type.ts
@@ -45,3 +45,22 @@ export interface LivePracticeSessionSetupType {
   totalPhrases?: number;
 
 }
+
+export interface TokenUsageType {
+  total_tokens: number;
+  input_tokens: number;
+  output_tokens: number;
+  input_token_details: {
+    cached_tokens: number;
+    text_tokens: number;
+    audio_tokens: number;
+    cached_tokens_details: {
+      text_tokens: number;
+      audio_tokens: number;
+    };
+  }
+  output_token_details: {
+    text_tokens: number;
+    audio_tokens: number;
+  }
+}
