fb69dd54e002d7af4c49b34dad9db686b4cff48f
SomiVista <somayerohani06@gmail.com>
2025-04-29 14:08:43 +0300

    feat: add Sidebar component for profile navigation with tabs for Profile, Subscription, and Billing

---
+++ frontend/components/profile/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+    <Card class="shadow-none">
+        <div class="flex flex-col">
+            <div
+                class="flex cursor-pointer items-center text-center"
+                :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'settings' }"
+                @click="$emit('update:activeTab', 'settings')"
+            >
+                <Icon name="IconUser" />
+                <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Profile</h3>
+            </div>
+
+            <div class="my-4 h-px w-full border-b border-[#e0e6ed] dark:border-[#1b2e4b]"></div>
+            <div
+                class="flex cursor-pointer items-center text-center"
+                :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'subscription' }"
+                @click="$emit('update:activeTab', 'subscription')"
+            >
+                <Icon name="IconCashBanknotes" />
+                <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Subscription</h3>
+            </div>
+            <div class="my-4 h-px w-full border-b border-[#e0e6ed] dark:border-[#1b2e4b]"></div>
+            <div
+                class="flex cursor-pointer items-center text-center"
+                :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'billing' }"
+                @click="$emit('update:activeTab', 'billing')"
+            >
+                <Icon name="IconClipboardText" />
+                <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Billing</h3>
+            </div>
+        </div>
+    </Card>
+</template>
+
+<script lang="ts" setup>
+    import { Card, Icon } from '@codebridger/lib-vue-components/elements.ts';
+
+    defineProps({
+        activeTab: {
+            type: String,
+            required: true,
+        },
+    });
+
+    defineEmits(['update:activeTab']);
+</script>
