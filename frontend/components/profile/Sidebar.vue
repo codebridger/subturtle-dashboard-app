d7ce8f8eb342052393484da5ae053970560a996f
SomiVista <somayerohani06@gmail.com>
2025-04-29 17:50:18 +0300

    #86et9akmr - refactor: enhance Sidebar component layout and navigation buttons for improved user experience

--- frontend/components/profile/Sidebar.vue
+++ frontend/components/profile/Sidebar.vue
@@ -1,32 +1,58 @@
 <template>
-    <Card class="shadow-none">
-        <div class="flex flex-col">
-            <NuxtLink to="/profile/settings">
-                <div class="flex cursor-pointer items-center text-center" :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'settings' }">
-                    <Icon name="IconUser" />
-                    <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Profile</h3>
-                </div>
-            </NuxtLink>
-
+    <div class="panel w-[240px] flex-none space-y-4 overflow-hidden p-4">
+        <div class="flex h-full flex-col">
+            <div class="flex items-center text-center">
+                <Icon name="IconUser" class="shrink-0" />
+                <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Profile</h3>
+            </div>
             <div class="my-4 h-px w-full border-b border-[#e0e6ed] dark:border-[#1b2e4b]"></div>
 
-            <NuxtLink to="/profile/subscription">
-                <div class="flex cursor-pointer items-center text-center" :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'subscription' }">
-                    <Icon name="IconCashBanknotes" />
-                    <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Subscription</h3>
-                </div>
-            </NuxtLink>
+            <client-only>
+                <div class="relative grow ltr:-mr-3.5 ltr:pr-3.5 rtl:-ml-3.5 rtl:pl-3.5">
+                    <div class="space-y-1">
+                        <NuxtLink to="/profile/settings">
+                            <button
+                                type="button"
+                                class="flex h-10 w-full items-center justify-between rounded-md p-2 font-medium hover:bg-white-dark/10 hover:text-primary dark:hover:bg-[#181F32] dark:hover:text-primary"
+                                :class="{ 'bg-gray-100 text-primary dark:bg-[#181F32] dark:text-primary': activeTab === 'settings' }"
+                            >
+                                <div class="flex items-center">
+                                    <Icon name="IconSettings" class="shrink-0" />
+                                    <div class="ltr:ml-3 rtl:mr-3">Settings</div>
+                                </div>
+                            </button>
+                        </NuxtLink>
 
-            <div class="my-4 h-px w-full border-b border-[#e0e6ed] dark:border-[#1b2e4b]"></div>
+                        <NuxtLink to="/profile/subscription">
+                            <button
+                                type="button"
+                                class="flex h-10 w-full items-center justify-between rounded-md p-2 font-medium hover:bg-white-dark/10 hover:text-primary dark:hover:bg-[#181F32] dark:hover:text-primary"
+                                :class="{ 'bg-gray-100 text-primary dark:bg-[#181F32] dark:text-primary': activeTab === 'subscription' }"
+                            >
+                                <div class="flex items-center">
+                                    <Icon name="IconCashBanknotes" class="shrink-0" />
+                                    <div class="ltr:ml-3 rtl:mr-3">Subscription</div>
+                                </div>
+                            </button>
+                        </NuxtLink>
 
-            <NuxtLink to="/profile/billing">
-                <div class="flex cursor-pointer items-center text-center" :class="{ 'bg-gray-100 dark:bg-gray-800': activeTab === 'billing' }">
-                    <Icon name="IconClipboardText" />
-                    <h3 class="text-lg font-semibold ltr:ml-3 rtl:mr-3">Billing</h3>
+                        <NuxtLink to="/profile/billing">
+                            <button
+                                type="button"
+                                class="flex h-10 w-full items-center justify-between rounded-md p-2 font-medium hover:bg-white-dark/10 hover:text-primary dark:hover:bg-[#181F32] dark:hover:text-primary"
+                                :class="{ 'bg-gray-100 text-primary dark:bg-[#181F32] dark:text-primary': activeTab === 'billing' }"
+                            >
+                                <div class="flex items-center">
+                                    <Icon name="IconClipboardText" class="shrink-0" />
+                                    <div class="ltr:ml-3 rtl:mr-3">Billing</div>
+                                </div>
+                            </button>
+                        </NuxtLink>
+                    </div>
                 </div>
-            </NuxtLink>
+            </client-only>
         </div>
-    </Card>
+    </div>
 </template>
 
 <script lang="ts" setup>
